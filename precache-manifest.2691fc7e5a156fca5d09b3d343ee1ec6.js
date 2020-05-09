4e6336e0b4fa40cad73f0dac46fab141a5dc2642
guychien <guychienll@gmail.com>
2020-05-09 22:50:49 +0800

    Updates

---
+++ precache-manifest.2691fc7e5a156fca5d09b3d343ee1ec6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c9b7cc668ec8fcd200eeb82d5742f95",
+    "url": "/aqi-index-service/index.html"
+  },
+  {
+    "revision": "88ff82badd3836d619dc",
+    "url": "/aqi-index-service/static/css/main.a1f92bb1.chunk.css"
+  },
+  {
+    "revision": "d0987abd2e8976e8dfbd",
+    "url": "/aqi-index-service/static/js/2.4c61470f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aqi-index-service/static/js/2.4c61470f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88ff82badd3836d619dc",
+    "url": "/aqi-index-service/static/js/main.c3e5e0dc.chunk.js"
+  },
+  {
+    "revision": "729bef7302a7a22a8e77",
+    "url": "/aqi-index-service/static/js/runtime-main.149e0c85.js"
+  }
+]);
